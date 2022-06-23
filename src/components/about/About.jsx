3e90d7b86c170febd22c4da212931b8e04070f47
Ahmed-Saad-j <106861742+Ahmed-Saad-J@users.noreply.github.com>
2022-06-23 16:50:44 +0200

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import "./about.css"
-import me from "../../assets/me.jpeg"
+import React from "react";
+import "./about.css";
+import me from "../../assets/me.jpeg";
 const About = () => {
   return (
-  <section id='about'>
-    <h5>Get To Know</h5>
-    <h1>About Me</h1>
-    <div className="container about__container">
-      <div className="about__me">
-        <div className="about__me-image">
-          <img src={me} alt="my picure" />
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h1>About Me</h1>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt="my picure" />
+          </div>
+        </div>
+        <div className="about__content">
+          <p>
+            Hello! My name is Ahmed saad. I am an informatics and computer
+            science student who specializes in software engineering in the
+            Britich university in egypt, I am also a full-stack developer with a
+            passion for back-end development, and I always enjoy learning new
+            things in my free time.{" "}
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
-      <div className="about__content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet mollitia rem soluta quaerat reiciendis asperiores nulla veritatis animi? Ea quod exercitationem necessitatibus delectus nobis a doloribus quos eaque, tempore inventore.</p>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-      </div>
-    </div>
-  </section>
-    )
-}
+    </section>
+  );
+};
 
-export default About
+export default About;
